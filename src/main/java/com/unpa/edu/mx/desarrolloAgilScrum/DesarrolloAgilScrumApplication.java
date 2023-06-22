34ac8b149dad0479c9556dd11ff971ae7f6087a0
JoseLeviRivera <joselevirivera@gmail.com>
2023-06-21 23:09:08 -0600

    Add: Se creo el proyecto

---
+++ src/main/java/com/unpa/edu/mx/desarrolloAgilScrum/DesarrolloAgilScrumApplication.java
@@ -0,0 +1,13 @@
+package com.unpa.edu.mx.desarrolloAgilScrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloAgilScrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloAgilScrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unpa/edu/mx/desarrolloAgilScrum/DesarrolloAgilScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpa.edu.mx.desarrolloAgilScrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloAgilScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
